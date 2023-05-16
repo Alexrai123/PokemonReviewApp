6c6291a9a6e64ac361fbbd09771c11bbc2ec6d76
Alexrai123 <alexhudita@gmail.com>
2023-05-16 22:58:30 +0300

    Add project files.

---
+++ PokemonReviewApp/Helper/MappingProfiles.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Pokemon, PokemonDto>();
+            CreateMap<PokemonDto, Pokemon>();
+
+            CreateMap<Category, CategoryDto>();
+            CreateMap<CategoryDto, Category>();
+
+            CreateMap<CountryDto, Country>();
+            CreateMap<Country, CountryDto>();
+
+            CreateMap<OwnerDto, Owner>();
+            CreateMap<Owner, OwnerDto>();
+
+            CreateMap<Review, ReviewDto>();
+            CreateMap<ReviewDto, Review>();
+
+            CreateMap<Reviewer, ReviewerDto>();
+            CreateMap<ReviewerDto, Reviewer>();
+        }
+    }
+}
